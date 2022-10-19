c56c1442f70a8235e0bbfa8fce71af7f74caae09
Sylwia <g.sylwia@vp.pl>
2022-10-19 23:04:46 +0200

    fix validation

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-
 const getCleanedZipCode = (value) => {
-    
+  
     if (typeof value === "number") {
-		value = String(value);
-	}
-    
-    const valueLength = value.length >= 5 && value.length <= 6
-   
-    if (valueLength && value.includes(" ")) {
-		console.log(value.replace(" ", "-"))
-    } else if ( valueLength && value.includes("")) {
-  		if (value.includes("-")) {
-            console.log(value)
-            return value
-        }
-		console.log(value.substring(0, 2) + "-" + value.substring(2, 5))
-	}  else {
-        console.log("")
+        value = String(value);
     }
+
+    if (value.length <= 4 || value.length >= 7) {
+        return ""
+    }
+
+    const removeWhiteSpaces = value.replace(/\s/g, '')
+  
+	if (removeWhiteSpaces.length === 5) {
+  		return removeWhiteSpaces.substring(0, 2) + "-" + removeWhiteSpaces.substring(2, 5)
+	}
+  
+    if (value.length === 6) {
+       const indexChar= value.indexOf("-")
+       const newArr = value.split("")
+       const removeChar = newArr.splice(indexChar, 1)
+       const newStr = newArr.join("")      
+       return newStr.substring(0, 2) + "-" + newStr.substring(2, 5)
+    }  
 }
 
-getCleanedZipCode("34-123")
+console.log(getCleanedZipCode("12-345"))
