ec06f080975f2a1badb3aae38116b2100e86a269
Sylwia <g.sylwia@vp.pl>
2022-10-20 12:31:20 +0200

    fix code and add validation first

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ const getCleanedZipCode = (value) => {
     if (typeof value === "number") {
         value = String(value);
     }
+    
+    value = value.replace(/\s/g, '')
+
+    if (value.includes("-")) {
+     	 const indexChar= value.indexOf("-")
+        const newArr = value.split("")
+        const removeChar = newArr.splice(indexChar, 1)
+        const newStr = newArr.join("")  
+        value = newStr
+    }   
 
     if (value.length <= 4 || value.length >= 7) {
         return ""
     }
-
-    const removeWhiteSpaces = value.replace(/\s/g, '')
   
-	if (removeWhiteSpaces.length === 5) {
-  		return removeWhiteSpaces.substring(0, 2) + "-" + removeWhiteSpaces.substring(2, 5)
-	}
-  
-    if (value.length === 6) {
-       const indexChar= value.indexOf("-")
-       const newArr = value.split("")
-       const removeChar = newArr.splice(indexChar, 1)
-       const newStr = newArr.join("")      
-       return newStr.substring(0, 2) + "-" + newStr.substring(2, 5)
-    }  
+    if (value.length === 5) {
+        return value.substring(0, 2) + "-" + value.substring(2, 5)
+    }
 }
 
-console.log(getCleanedZipCode("12-345"))
+console.log(getCleanedZipCode("-2123"))
